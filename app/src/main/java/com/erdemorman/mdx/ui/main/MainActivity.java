e5a1aa80ab2118c88a3b2745f920cf47e5e52af2
Erdem Orman <ormanerdem@gmail.com>
2016-05-09 22:32:15 +0300

    Unnecessary drawables are removed and navigation menu updated

--- app/src/main/java/com/erdemorman/mdx/ui/main/MainActivity.java
+++ app/src/main/java/com/erdemorman/mdx/ui/main/MainActivity.java
@@ -77,17 +77,9 @@ public class MainActivity extends BaseActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_colors) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_icons) {
 
         }
 
